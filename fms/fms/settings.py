9036f6c4b4bc9405ad6b413475f1ef9ac361a15a
Matheshhp24 <134129041+Matheshhp24@users.noreply.github.com>
2024-10-02 22:53:48 +0530

    s3access

--- fms/fms/settings.py
+++ fms/fms/settings.py
@@ -174,11 +174,20 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'leave_documents')
 
 #django_heroku.settings(locals())
 
+
+STORAGES = {
+
+    # Media file (image) management  
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+    },
+   
+}
+
 AWS_ACCESS_KEY_ID = 'AKIAVVZPCXTDTVR4U46P'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'flms-documents'
 AWS_S3_REGION_NAME = 'us-west-2'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 
