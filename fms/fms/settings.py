36dd6060e57f14938111d45baabb9c3deff9ca5b
Matheshhp24 <134129041+Matheshhp24@users.noreply.github.com>
2024-10-02 22:37:03 +0530

    all changes

--- fms/fms/settings.py
+++ fms/fms/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.postgres',
 ]
 
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware', 
@@ -114,7 +116,6 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
