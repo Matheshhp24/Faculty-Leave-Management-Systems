28ad60cd75b191d2aa34a2829d56458c6909d4db
Matheshhp24 <134129041+Matheshhp24@users.noreply.github.com>
2024-10-02 23:34:09 +0530

    media commit

--- fms/fms/settings.py
+++ fms/fms/settings.py
@@ -167,9 +167,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Media files (Documents)
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'leave_documents')
-
 
 
 #django_heroku.settings(locals())
@@ -182,4 +181,6 @@ AWS_STORAGE_BUCKET_NAME = 'flms-documents'
 AWS_S3_REGION_NAME = 'us-west-2'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+
 
