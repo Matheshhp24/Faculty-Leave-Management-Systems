e4d3d10965111be21d4aa025aaff9f6161181e2d
Matheshhp24 <134129041+Matheshhp24@users.noreply.github.com>
2024-10-02 22:42:39 +0530

    dleted

--- fms/fms/settings.py
+++ fms/fms/settings.py
@@ -174,8 +174,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'leave_documents')
 
 #django_heroku.settings(locals())
 
-AWS_ACCESS_KEY_ID = 'AKIAVVZPCXTDTVR4U46P'
-AWS_SECRET_ACCESS_KEY = ''
+
 AWS_STORAGE_BUCKET_NAME = 'flms-documents'
 AWS_S3_REGION_NAME = 'us-west-2'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
